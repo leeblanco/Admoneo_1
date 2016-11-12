93e0b6d39e56178562f4eaf6139ef1bcd7a416b9
dimaz <lee.blanco.2@gmail.com>
2016-11-12 18:09:20 +0400

    Modified web.xml correct package to find WicketApplication class. Todo insert is now working

--- src/main/java/com/gre/model/Todo.java
+++ src/main/java/com/gre/model/Todo.java
@@ -1,13 +1,13 @@
 package com.gre.model;
 
+import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.GenerationType;
-
-import java.util.Date;
 
 /**
  * 
